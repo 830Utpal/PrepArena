a853fd7fa209525699115061f9c3a17a607624b7
830Utpal <lakshman2016yadav@gmail.com>
2025-03-22 20:07:43 +0530

    agent interview page

---
+++ components/Agent.tsx
@@ -0,0 +1,28 @@
+import Image from 'next/image'
+import React from 'react'
+
+const Agent = ({userName}:AgentProps) => {
+    const isSpeaking=true;
+  return (
+    <>
+   <div className='call-view'>
+    <div className='card-interviewer'>
+        <div className='avatar'>
+           <Image src="/ai-avatar.png" alt="vapi" width={65} height={54} className='"object-cover'/>
+           {isSpeaking && <span className='"animate-speak'/>}
+        </div>
+        <h3>AI Interviewer</h3>
+    </div>
+    <div className='"card-border'>
+         <Image src="/user-avatar.png" alt="user avatar" width={540} height={540} className='rounded-full object-cover size-[120px]'/>
+         <h3>{userName}</h3>
+    </div>
+   </div>
+   <div className='w-full flex-justify-center'>
+
+   </div>
+   </>
+  )
+}
+
+export default Agent
